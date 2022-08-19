97e6cf4882f70f8fa6afd0dd00c255a2c900d4a7
Martin Gonzalez <martin.gonzalez@rewardstyle.com>
2022-08-19 08:57:52 -0400

    tests: expand and update snapshots

--- src/lib/__tests__/fixtures/jest.test.js
+++ src/lib/__tests__/fixtures/jest.test.js
@@ -2,6 +2,9 @@ describe('Some describe title 1', () => {
     // some code 1
     /* eslint-disable-next-line */
     describe.only('Nested describe 1', () => {
+        // some setup
+        // some more setup
+
         it('does this and that', () => {
             // code of what it does
             expect(true).toBeTruthy()
